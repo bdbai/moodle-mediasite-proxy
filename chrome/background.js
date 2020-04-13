0de698ca06ce26834695af999f953fc6652e93db
bdbai <bdbaiapp@163.com>
2020-04-13 15:37:44 +0800

    Fix course view

--- chrome/background.js
+++ chrome/background.js
@@ -5,12 +5,106 @@ let currentWindow = undefined
 
 chrome.windows.getCurrent(w => currentWindow = w)
 
+/**
+ * @param {string} txt
+ * @returns {string}
+ */
+const getActionFromHtml = txt => txt.match(/action=("|')(.*?)("|')/)[2]
+
+/**
+ * @param {string} txt 
+ * @returns {URLSearchParams}
+ */
+const getUrlSearchParamsFromHtml = txt => new URLSearchParams(
+    Array
+        .from(txt.matchAll(/name=("|')(.*?)("|').*?value=("|')(.*?)("|')/g))
+        .map(([_1, _2, name, _3, _4, value, _5]) => [name, value]))
+
+/**
+ * @param {string} moodleId 
+ */
+async function getPlayerOptions(moodleId) {
+    // Moodle ID => Mediasite credentials
+    /** @type {string} */
+    const moodleLandingTxt = await fetch(`https://l.xmu.edu.my/mod/mediasite/content_launch.php?id=${moodleId}&coverplay=1`, {
+        credentials: 'include'
+    })
+        .then(res => res.text())
+    const coverPlayUrl = getActionFromHtml(moodleLandingTxt)
+    const moodleLandingParams = getUrlSearchParamsFromHtml(moodleLandingTxt)
+    // Mediasite credentials => auth ticket
+    const coverPlayRes = await fetch(coverPlayUrl, {
+        method: 'post',
+        body: moodleLandingParams,
+        credentials: 'omit'
+    })
+        .then(res => res.text())
+    const launcherUrl = getActionFromHtml(coverPlayRes)
+    const launcherParams = getUrlSearchParamsFromHtml(coverPlayRes)
+    const mediasiteId = moodleLandingParams.get('mediasiteid')
+    // Auth ticket => cookie stored
+    launcherParams.set('cookiesEnabled', true)
+    launcherParams.set('CookieSupport', true)
+    launcherParams.set('IsBetterCookieSupportForm', true)
+    await fetch(launcherUrl, {
+        method: 'post',
+        body: launcherParams
+    })
+    // Finally,
+    const getPlayerOptionsRes = await fetch("https://mymedia.xmu.edu.cn/Mediasite/PlayerService/PlayerService.svc/json/GetPlayerOptions", {
+        method: 'post',
+        body: JSON.stringify({
+            getPlayerOptionsRequest: {
+                ResourceId: mediasiteId,
+                QueryString: '',
+                UseScreenReader: false,
+                UrlReferrer: launcherUrl
+            }
+        }),
+        credentials: 'include',
+        headers: {
+            'content-type': 'application/json'
+        }
+    })
+        .then(res => res.json())
+
+    const {
+        d: {
+            CoverageEvents: coverages,
+            Presentation: {
+                Title: title,
+                Streams: streams,
+                Duration: duration
+            },
+            PresentationBookmark: bookmark
+        }
+    } = getPlayerOptionsRes
+    const directUrls = streams
+        .filter(s => s.VideoUrls.length > 0)
+        .map(s => s.VideoUrls[0].Location)
+    const slideStreams = streams
+        .filter(s => s.StreamType === 2)
+    return {
+        type: 'getPlayerOptions',
+        directUrls,
+        slideStreams,
+        title,
+        mediasiteId,
+        coverages,
+        duration,
+        bookmark
+    }
+}
+
 chrome.runtime.onMessage.addListener((msg, _sender, sendResponse) => {
     const { type } = msg
     if (typeof currentWindow === 'undefined') {
         return
     }
     switch (type) {
+        case 'getPlayerOptions':
+            getPlayerOptions(msg.moodleId).then(sendResponse)
+            return true
         case 'getWindowState':
             const gotState = currentWindow.state || 'maximized'
             sendResponse(gotState)
@@ -20,6 +114,7 @@ chrome.runtime.onMessage.addListener((msg, _sender, sendResponse) => {
             console.debug('setwindowstate', msg)
             const { state } = msg
             chrome.windows.update(currentWindow.id, { state }, _w => sendResponse())
+            return true
     }
 })
 
--- chrome/courseView.js
+++ chrome/courseView.js
@@ -1,156 +1,5 @@
-/**
- * @template T
- * @param {T[]} arr
- * @param {(item: T) => boolean} predicate
- * @returns {T[]}
- */
-function skipUntil(arr, predicate) {
-    for (let i = 0; i < arr.length; i++) {
-        if (predicate(arr[i])) {
-            return arr.slice(i)
-        }
-    }
-    return []
-}
-
-/**
- * @template T
- * @param {PromiseLike<T>[]} arr
- * @param {(item: T) => boolean} predicate
- * @return {Promise<T | undefined>}
- */
-async function findAsync(arr, predicate) {
-    for (const item of arr) {
-        const result = await item
-        if (predicate(result)) {
-            return result
-        }
-    }
-    return undefined
-}
-
 const $mediaLis = Array
     .from(document.querySelectorAll('li.activity.mediasite.modtype_mediasite'))
-
-/**
- * @type {Promise<[string, HTMLDivElement]>[]}
- */
-const mediaElements = $mediaLis
-    .map($l => {
-        const $content = $l.querySelector('div.mediasite-content')
-        const id = $l.id.substr(7) // module-76543
-        return fetch(`https://l.xmu.edu.my/mod/mediasite/content_launch.php?id=${id}&coverplay=1`, {
-            credentials: 'include'
-        })
-            .then(res => res.text())
-            .then(res => skipUntil(skipUntil(res
-                .split('\n'), line => line.includes('name="mediasiteid"'))[0]
-                .split('"'), field => field === ' value=')[1])
-            .then(mediasiteId => [mediasiteId, $content])
-    })
-
-window.addEventListener('message', async e => {
-    console.debug('Got message', e.data)
-    if (e.data.type === 'getPlayerOptions') {
-        const {
-            directUrls,
-            slideStreams,
-            title,
-            mediasiteId,
-            coverages, // second!
-            duration,
-            bookmark
-        } = e.data
-        const con = await findAsync(mediaElements, ([mId, _$c]) => mId === mediasiteId);
-        if (!con) {
-            console.debug('Cannot find corresponding media element from message', e.data)
-            return
-        }
-        const [_, $con] = con
-
-        // Append media info
-        const $header = document.createElement('h4')
-        $header.innerText = 'Media information'
-        $con.appendChild($header)
-
-        $con.appendChild(document.createElement('hr'))
-
-        const $title = document.createElement('p')
-        $title.innerText = 'Title: ' + title
-        $con.appendChild($title)
-
-        const $urlText = document.createElement('h5')
-        $urlText.innerText = 'Direct URLs'
-        $con.appendChild($urlText)
-
-        const $urlList = document.createElement('ul')
-        for (const url of directUrls) {
-            const $li = document.createElement('li')
-            const $a = document.createElement('a')
-            $a.href = url
-            $a.innerText = url
-            $li.appendChild($a)
-            $urlList.appendChild($li)
-        }
-        $con.appendChild($urlList)
-
-        for (const slideStream of slideStreams) {
-            const $copySlideDataBtn = document.createElement('button')
-            $copySlideDataBtn.innerText = 'Copy slide data'
-            $copySlideDataBtn.addEventListener('click', ev => {
-                ev.preventDefault()
-                navigator.clipboard
-                    .writeText(JSON.stringify(slideStream))
-                    .then(() => alert('Slide data copied'))
-                    .catch(e => alert('Cannot copy slide data: ' + e.toString()))
-            })
-            $con.appendChild($copySlideDataBtn)
-        }
-
-        // Show completeness
-        let $li = $con.parentElement
-        while ($li && $li.tagName !== 'LI') {
-            $li = $li.parentElement
-        }
-        if (!$li) {
-            return
-        }
-        let appendix = ' '
-        if (bookmark && bookmark.position) {
-            const { position } = bookmark
-            const progress = position / duration
-            appendix += `[bookmark at ${
-                Math.floor(position / 6e4)}:${
-                Math.floor(position % 6e4 / 1e3)}(${
-                progress.toLocaleString('en-US', {
-                    style: 'percent',
-                    maximumFractionDigits: 2
-                })})] `
-        }
-        const $instanceNameNode = $li.querySelector('span.instancename')
-        const $instanceNameTextNode = $instanceNameNode.childNodes[0]
-
-        // Assume coverages do not overlap
-        const totalSeconds = Math.floor(duration / 1e3)
-        let coveredSeconds = 0
-        for (const {
-            Duration: duration,
-            StartTime: startTime
-        } of coverages) {
-            const endTime = Math.min(duration + startTime, totalSeconds)
-            coveredSeconds += endTime - startTime
-        }
-        appendix += `[Est. completeness = ${
-            Math.min(1, coveredSeconds / totalSeconds)
-                .toLocaleString('en-US', {
-                    style: 'percent',
-                    maximumFractionDigits: 2
-                })
-            }]`
-        $instanceNameNode.setAttribute('data-original-text', $instanceNameTextNode.textContent)
-        $instanceNameTextNode.textContent += appendix //` [bookmark at 1:3(5%)][Est. completeness = %]`
-    }
-})
 
 /**
  * @returns {Promise<string>}
@@ -168,13 +17,124 @@ const setWindowState = state => chrome.runtime.sendMessage({
     state
 })
 
+/**
+ * @param {string} moodleId 
+ * @returns {Promise<any>}
+ */
+const getPlayerOptionsAsync = moodleId => new Promise((resolve, _reject) => chrome.runtime.sendMessage({
+    type: 'getPlayerOptions',
+    moodleId
+}, resolve))
+
 let isFullscreen = false
+
+/**
+ * @type {Promise<{ extractInfo: boolean }>}
+ */
+const extractInfoAsync = new Promise((resolve, _reject) => chrome.storage.sync.get({ extractInfo: true }, resolve))
+
+/**
+ * 
+ * @param {Element} $el 
+ */
+async function collectFromGetPlayerOptions($el) {
+    const id = $el.id.substr(7) // module-76543
+    const playerOptions = await getPlayerOptionsAsync(id)
+    const {
+        directUrls,
+        slideStreams,
+        title,
+        mediasiteId,
+        coverages, // second!
+        duration,
+        bookmark
+    } = playerOptions
+
+    // Append media info
+    const $header = document.createElement('h4')
+    $header.innerText = 'Media information'
+    $el.appendChild($header)
+
+    $el.appendChild(document.createElement('hr'))
+
+    const $title = document.createElement('p')
+    $title.innerText = 'Title: ' + title
+    $el.appendChild($title)
+
+    const $urlText = document.createElement('h5')
+    $urlText.innerText = 'Direct URLs'
+    $el.appendChild($urlText)
+
+    const $urlList = document.createElement('ul')
+    for (const url of directUrls) {
+        const $li = document.createElement('li')
+        const $a = document.createElement('a')
+        $a.href = url
+        $a.innerText = url
+        $li.appendChild($a)
+        $urlList.appendChild($li)
+    }
+    $el.appendChild($urlList)
+
+    for (const slideStream of slideStreams) {
+        const $copySlideDataBtn = document.createElement('button')
+        $copySlideDataBtn.innerText = 'Copy slide data'
+        $copySlideDataBtn.addEventListener('click', ev => {
+            ev.preventDefault()
+            navigator.clipboard
+                .writeText(JSON.stringify(slideStream))
+                .then(() => alert('Slide data copied'))
+                .catch(e => alert('Cannot copy slide data: ' + e.toString()))
+        })
+        $el.appendChild($copySlideDataBtn)
+    }
+
+    // Show completeness
+    let appendix = ' '
+    if (bookmark && bookmark.position) {
+        const { position } = bookmark
+        const progress = position / duration * 1000
+        appendix += `[bookmark at ${
+            Math.floor(position / 60)}:${
+            Math.floor(position % 60)}(${
+            progress.toLocaleString('en-US', {
+                style: 'percent',
+                maximumFractionDigits: 2
+            })})] `
+    }
+    const $instanceNameNode = $el.querySelector('span.instancename')
+    const $instanceNameTextNode = $instanceNameNode.childNodes[0]
+
+    // Assume coverages do not overlap
+    const totalSeconds = Math.floor(duration / 1e3)
+    let coveredSeconds = 0
+    for (const {
+        Duration: duration,
+        StartTime: startTime
+    } of coverages) {
+        const endTime = Math.min(duration + startTime, totalSeconds)
+        coveredSeconds += endTime - startTime
+    }
+    appendix += `[Est. completeness = ${
+        Math.min(1, coveredSeconds / totalSeconds)
+            .toLocaleString('en-US', {
+                style: 'percent',
+                maximumFractionDigits: 2
+            })
+        }]`
+    $instanceNameNode.setAttribute('data-original-text', $instanceNameTextNode.textContent)
+    $instanceNameTextNode.textContent += appendix //` [bookmark at 1:3(5%)][Est. completeness = %]`
+}
 
 /**
  * @type {string}
  */
 let defaultWindowState = 'maximized'
 !function () {
+    // Collect information from GetPlayerOptions
+    extractInfoAsync.then(({ extractInfo }) => extractInfo
+        && Promise.all($mediaLis.map(collectFromGetPlayerOptions)))
+
     const $style = document.createElement('style')
     $style.innerHTML = `
     html.mediasite-proxy-fullscreen {
@@ -219,6 +179,8 @@ let defaultWindowState = 'maximized'
         border: none;
     }`
     document.head.appendChild($style)
+
+    // Inject full screen btn
     const cons = document.getElementsByClassName('mediasite-content')
     for (const $con of Array.from(cons)) {
         const $control = document.createElement('div')
